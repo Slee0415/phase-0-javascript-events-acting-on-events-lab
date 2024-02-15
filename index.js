b34c2a8d0f52f491238eb09c9d44bb6197244a8d
Shane Lee <slee0415@gmail.com>
2024-02-15 04:45:12 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${Math.max(left - 1, 0)}px`; // Ensure left doesn't go below 0
+    }
+}
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`; // Move to the right by 1 pixel
+    }
+}
